f4d4dbd92f15c4c82d86169bc5de1a37866d00cc
Gigi Kenneth <gigikenneth7@gmail.com>
2023-12-28 12:24:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,10 +123,10 @@ with st.sidebar:
             st.error(f"Failed to load the example dataset. Error: {e}")
 
     # Check if data is loaded for year selection
-    if 'data' in locals():
-        min_year = int(data['Date Added'].dt.year.min())
-        max_year = int(data['Date Added'].dt.year.max())
-        year = st.select_slider("Select Year", options=range(min_year, max_year + 1), value=max_year)
+if 'data' in locals():
+    min_year = int(data['Date Added'].dt.year.min())
+    max_year = int(data['Date Added'].dt.year.max())
+    year = st.select_slider("Select Year", options=range(min_year, max_year + 1), value=max_year)
 
     st.markdown('<a href="https://github.com/gigikenneth/goodreads" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" width="30" height="30" alt="GitHub"></a>', unsafe_allow_html=True)
     st.sidebar.markdown('Made chaotically at 3am🌪️ by [Gigi](https://github.com/gigikenneth)')
