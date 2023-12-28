b3ac15d7bf10356d520abd0ac6426201d0370ac5
Gigi Kenneth <gigikenneth7@gmail.com>
2023-12-27 19:01:54 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 from collections import Counter
 import datetime
 
@@ -12,26 +11,22 @@ def load_data(uploaded_file):
     data['Date Added'] = pd.to_datetime(data['Date Added'], errors='coerce')
     return data
 
-# Function for cumulative books plot
+# Function for cumulative books plot using plotly
 def plot_cumulative_books(data):
     data_sorted = data.sort_values('Date Added')
     data_sorted['Cumulative Books'] = range(1, len(data_sorted) + 1)
-    plt.figure(figsize=(12, 6))
-    plt.plot(data_sorted['Date Added'], data_sorted['Cumulative Books'])
-    plt.title('Cumulative Number of Books Added Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('Total Number of Books')
-    plt.grid(True)
+    fig = px.line(data_sorted, x='Date Added', y='Cumulative Books', 
+                  title='Cumulative Number of Books Added Over Time',
+                  labels={'Date Added': 'Date', 'Cumulative Books': 'Total Number of Books'})
+    return fig
 
-# Function for the distribution of book lengths
+# Function for the distribution of book lengths using plotly
 def plot_book_lengths(data):
     valid_page_data = data[data['Number of Pages'].notna() & (data['Number of Pages'] > 0)]
-    plt.figure(figsize=(12, 6))
-    sns.histplot(valid_page_data['Number of Pages'], bins=30, kde=True)
-    plt.title('Distribution of Book Lengths (Number of Pages)')
-    plt.xlabel('Number of Pages')
-    plt.ylabel('Number of Books')
-    plt.grid(True)
+    fig = px.histogram(valid_page_data, x='Number of Pages', 
+                       title='Distribution of Book Lengths (Number of Pages)',
+                       labels={'Number of Pages': 'Number of Pages'})
+    return fig
 
 # Main Streamlit app
 st.title("Goodreads Library Analysis")
@@ -42,15 +37,15 @@ if uploaded_file is not None:
 
     # Cumulative Books Plot
     st.subheader("Cumulative Number of Books Added Over Time")
-    plot_cumulative_books(data)
-    st.pyplot(plt)
+    fig1 = plot_cumulative_books(data)
+    st.plotly_chart(fig1)
     st.markdown("### Insight")
     st.write("Your reading journey over time shows...")
 
     # Distribution of Book Lengths
     st.subheader("Distribution of Book Lengths")
-    plot_book_lengths(data)
-    st.pyplot(plt)
+    fig2 = plot_book_lengths(data)
+    st.plotly_chart(fig2)
     st.markdown("### Insight")
     st.write("The variety of book lengths in your library indicates...")
 
@@ -61,6 +56,5 @@ if uploaded_file is not None:
     books_this_year = data[data['Date Added'].dt.year == current_year]
     st.write(f"You've added {len(books_this_year)} books to your library in {current_year}!")
 
-# Run this code in your Python environment after installing streamlit using 'pip install streamlit'
+# Run this code in your Python environment after installing plotly and streamlit using 'pip install plotly streamlit'
 # You can start the app by running 'streamlit run your_script_name.py' in the terminal
-
