080eec0ffd3a460f130719ae7a86d859fc055e3f
Gigi Kenneth <gigikenneth7@gmail.com>
2023-12-27 19:21:58 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,25 +7,56 @@ import datetime
 # Function to load data
 def load_data(uploaded_file):
     data = pd.read_csv(uploaded_file)
-    # Perform any necessary data preprocessing here
     data['Date Added'] = pd.to_datetime(data['Date Added'], errors='coerce')
+    data['Date Read'] = pd.to_datetime(data['Date Read'], errors='coerce')
+    data['Year Published'] = pd.to_numeric(data['Year Published'], errors='coerce')
     return data
 
-# Function for cumulative books plot using plotly
-def plot_cumulative_books(data):
-    data_sorted = data.sort_values('Date Added')
-    data_sorted['Cumulative Books'] = range(1, len(data_sorted) + 1)
-    fig = px.line(data_sorted, x='Date Added', y='Cumulative Books', 
-                  title='Cumulative Number of Books Added Over Time',
-                  labels={'Date Added': 'Date', 'Cumulative Books': 'Total Number of Books'})
+# Function for distribution of book ratings
+def plot_book_ratings(data, year=None):
+    if year:
+        data = data[data['Date Added'].dt.year == year]
+    fig = px.histogram(data, x='My Rating', title=f'Distribution of Book Ratings {"in " + str(year) if year else ""}')
     return fig
 
-# Function for the distribution of book lengths using plotly
+# Function for most common authors
+def plot_common_authors(data, year=None):
+    if year:
+        data = data[data['Date Added'].dt.year == year]
+    author_counts = Counter(data['Author'])
+    authors_df = pd.DataFrame(author_counts.most_common(10), columns=['Author', 'Count'])
+    fig = px.bar(authors_df, x='Count', y='Author', title=f'Most Common Authors {"in " + str(year) if year else ""}', orientation='h')
+    return fig
+
+# Function for distribution of books by publication year
+def plot_books_by_publication_year(data):
+    fig = px.histogram(data, x='Year Published', title='Distribution of Books by Publication Year')
+    return fig
+
+# Function for cumulative books plot
+def plot_cumulative_books(data, year=None):
+    if year:
+        data = data[data['Date Added'].dt.year == year]
+    data_sorted = data.sort_values('Date Added')
+    data_sorted['Cumulative Books'] = range(1, len(data_sorted) + 1)
+    fig = px.line(data_sorted, x='Date Added', y='Cumulative Books', title=f'Cumulative Number of Books Added Over Time {"in " + str(year) if year else ""}')
+    return fig
+
+# Function for the distribution of book lengths
 def plot_book_lengths(data):
     valid_page_data = data[data['Number of Pages'].notna() & (data['Number of Pages'] > 0)]
-    fig = px.histogram(valid_page_data, x='Number of Pages', 
-                       title='Distribution of Book Lengths (Number of Pages)',
-                       labels={'Number of Pages': 'Number of Pages'})
+    fig = px.histogram(valid_page_data, x='Number of Pages', title='Distribution of Book Lengths (Number of Pages)')
+    return fig
+
+# Function for read vs. unread books
+def plot_read_unread_books(data, year=None):
+    if year:
+        data = data[data['Date Added'].dt.year == year]
+    read_books_count = (data['Read Count'] > 0).sum()
+    unread_books_count = (data['Read Count'] == 0).sum()
+    counts = [read_books_count, unread_books_count]
+    labels = ['Read Books', 'Unread Books']
+    fig = px.pie(names=labels, values=counts, title=f'Read vs. Unread Books {"in " + str(year) if year else ""}')
     return fig
 
 # Main Streamlit app
@@ -35,26 +66,55 @@ uploaded_file = st.file_uploader("Upload your Goodreads CSV", type="csv")
 if uploaded_file is not None:
     data = load_data(uploaded_file)
 
+    # Book Ratings Plot
+    st.subheader("Distribution of Book Ratings")
+    fig1 = plot_book_ratings(data)
+    st.plotly_chart(fig1)
+
+    # Book Ratings Plot for 2023
+    st.subheader("Distribution of Book Ratings in 2023")
+    fig2 = plot_book_ratings(data, year=2023)
+    st.plotly_chart(fig2)
+
+    # Most Common Authors Plot
+    st.subheader("Most Common Authors")
+    fig3 = plot_common_authors(data)
+    st.plotly_chart(fig3)
+
+    # Most Common Authors in 2023
+    st.subheader("Most Common Authors in 2023")
+    fig4 = plot_common_authors(data, year=2023)
+    st.plotly_chart(fig4)
+
+    # Books by Publication Year
+    st.subheader("Distribution of Books by Publication Year")
+    fig5 = plot_books_by_publication_year(data)
+    st.plotly_chart(fig5)
+
     # Cumulative Books Plot
     st.subheader("Cumulative Number of Books Added Over Time")
-    fig1 = plot_cumulative_books(data)
-    st.plotly_chart(fig1)
-    st.markdown("### Insight")
-    st.write("Your reading journey over time shows...")
+    fig6 = plot_cumulative_books(data)
+    st.plotly_chart(fig6)
 
-    # Distribution of Book Lengths
+    # Cumulative Books in 2023
+    st.subheader("Cumulative Number of Books Added in 2023")
+    fig7 = plot_cumulative_books(data, year=2023)
+    st.plotly_chart(fig7)
+
+    # Book Lengths Plot
     st.subheader("Distribution of Book Lengths")
-    fig2 = plot_book_lengths(data)
-    st.plotly_chart(fig2)
-    st.markdown("### Insight")
-    st.write("The variety of book lengths in your library indicates...")
+    fig8 = plot_book_lengths(data)
+    st.plotly_chart(fig8)
 
-    # Add other plots and insights here
+    # Read vs. Unread Books Plot
+    st.subheader("Read vs. Unread Books")
+    fig9 = plot_read_unread_books(data)
+    st.plotly_chart(fig9)
 
-    # Example of a dynamic insight
-    current_year = datetime.datetime.now().year
-    books_this_year = data[data['Date Added'].dt.year == current_year]
-    st.write(f"You've added {len(books_this_year)} books to your library in {current_year}!")
+    # Read vs. Unread Books in 2023
+    st.subheader("Read vs. Unread Books in 2023")
+    fig10 = plot_read_unread_books(data, year=2023)
+    st.plotly_chart(fig10)
 
 # Run this code in your Python environment after installing plotly and streamlit using 'pip install plotly streamlit'
 # You can start the app by running 'streamlit run your_script_name.py' in the terminal
